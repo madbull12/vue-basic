67c0d916b6e964698f4f4e5a68ad5ca2643ea606
andriankreasitech <huangandrian02@gmail.com>
2024-08-08 00:19:04 +0700

    json server installed

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import JobsView from '@/views/JobsView.vue';
-// import NotFoundView from '@/views/NotFoundView.vue';
-// import JobView from '@/views/JobView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
+import JobView from '@/views/JobView.vue';
 // import AddJobView from '@/views/AddJobView.vue';
 // import EditJobView from '@/views/EditJobView.vue';
 
@@ -17,6 +17,14 @@ const router = createRouter({
       path:"/jobs",
       name:"jobs",
       component:JobsView
+    },{
+      path:'/jobs/:id',
+      name:'job',
+      component:JobView
+    },{
+      path:'/:catchAll(.*)',
+      name:'not-found',
+      component:NotFoundView
     }
   ],
 });
