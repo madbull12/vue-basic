801c1e82fe670d1ddff1b49a10e2867e250eb7e0
andriankreasitech <huangandrian02@gmail.com>
2024-08-08 23:18:59 +0700

    job detail

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,10 +1,29 @@
-<script setup></script>
+<script setup>
+import { reactive, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import axios from 'axios'
+const route = useRoute();
+const jobId = route.params.id;
+const state = reactive({
+  job: {},
+  isLoading: true,
+});
+onMounted(async () => {
+  try {
+    const res = await axios.get(`http://localhost:5000/jobs/${jobId}`);
+    state.job = res.data;
+    console.log(res.data)
+  } catch (err) {
+    console.log(err);
+  } finally {
+    state.isLoading = false;
+  }
+});
+</script>
 
 <template>
-
-
   <!-- Go Back -->
-  <section>
+  <section v-if="!state.isLoading">
     <div class="container m-auto py-6 px-6">
       <a
         href="jobs.html"
@@ -14,7 +33,9 @@
       </a>
     </div>
   </section>
-
+  <div v-else>
+    Loading...
+  </div>
   <section class="bg-green-50">
     <div class="container m-auto py-10 px-6">
       <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
@@ -22,21 +43,21 @@
           <div
             class="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
           >
-            <div class="text-gray-500 mb-4">Full-Time</div>
-            <h1 class="text-3xl font-bold mb-4">Senior Vue Developer</h1>
+            <div class="text-gray-500 mb-4">{{ state.job.type }}</div>
+            <h1 class="text-3xl font-bold mb-4">{{ state.job.title }}</h1>
             <div
               class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
             >
               <i
                 class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
               ></i>
-              <p class="text-orange-700">Boston, MA</p>
+              <p class="text-orange-700">{{ state.job.location }}</p>
             </div>
           </div>
 
           <div class="bg-white p-6 rounded-lg shadow-md mt-6">
             <h3 class="text-green-800 text-lg font-bold mb-6">
-              Job Description
+              {{ state.job.description }}
             </h3>
 
             <p class="mb-4">
@@ -48,7 +69,7 @@
 
             <h3 class="text-green-800 text-lg font-bold mb-2">Salary</h3>
 
-            <p class="mb-4">$70k - $80K / Year</p>
+            <p class="mb-4">{{ state.job.salary }}</p>
           </div>
         </main>
 
@@ -58,13 +79,10 @@
           <div class="bg-white p-6 rounded-lg shadow-md">
             <h3 class="text-xl font-bold mb-6">Company Info</h3>
 
-            <h2 class="text-2xl">NewTek Solutions</h2>
+            <h2 class="text-2xl">sasa</h2>
 
             <p class="my-2">
-              NewTek Solutions is a leading technology company specializing in
-              web development and digital solutions. We pride ourselves on
-              delivering high-quality products and services to our clients while
-              fostering a collaborative and innovative work environment.
+              {{ state.job.company.description }}
             </p>
 
             <hr class="my-4" />
@@ -72,7 +90,7 @@
             <h3 class="text-xl">Contact Email:</h3>
 
             <p class="my-2 bg-green-100 p-2 font-bold">
-              contact@newteksolutions.com
+              <!-- {{ state.job.company.contactEmail }} -->
             </p>
 
             <h3 class="text-xl">Contact Phone:</h3>
@@ -98,4 +116,5 @@
       </div>
     </div>
   </section>
+
 </template>
